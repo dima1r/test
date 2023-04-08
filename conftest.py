e331018f9a2dc8504cccd38a48c2b2588484624e
dima1r <dr246029@gmail.com>
2023-04-08 12:24:58 +0300

    adding new files

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument('--start-maximized')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     yield driver
     driver.quit()
--- tests/test_elementary.py
+++ tests/test_elementary.py
@@ -1,8 +1,11 @@
 from pages.base_page import BasePage
 from time import sleep
 
-def test(driver):
+def test_main(driver):
     page = BasePage(driver, 'https://www.google.com')
     #page = BasePage(driver, 'google.com')
     page.open()
     sleep(5)
+    cur_url = driver.current_url
+    print(cur_url)
+    assert 'https://www.google.com' in cur_url, 'Page not found'
