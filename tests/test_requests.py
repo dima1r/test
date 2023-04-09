58604893ec7846c49062fd4db5817b3a4f777e63
dima1r <dr246029@gmail.com>
2023-04-09 20:13:52 +0300

    no changes

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -5,12 +5,16 @@ from consts import SERVICE_URL
 from sources.enums.global_enums import GlobalErrorMessages
 
 def test_get_req():
-    responce = requests.get(url = SERVICE_URL)
-    stat_code = responce.status_code
-    print(stat_code)
+    #responce = requests.get(url = SERVICE_URL)
+    #stat_code = responce.status_code
+    #print(responce)
     #print(responce.__getattribute__(name))
-    assert stat_code in range(200, 300), GlobalErrorMessages.WRONG_STATUS_CODE.value
+    # assert stat_code in range(200, 300), GlobalErrorMessages.WRONG_STATUS_CODE.value
+    headers = {'Accept': 'application/json'}
     try:
-        received_data = responce.json()
+        responce = requests.get(SERVICE_URL, headers = headers).json()
+        print(responce)
+        #received_data = responce.json()
     except JSONDecodeError:
         print('No JSON')
+    
