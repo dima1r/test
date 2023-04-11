03836a0d27af6eafd1894094519a00bc8ca4b7c8
dima1r <dr246029@gmail.com>
2023-04-11 23:31:33 +0300

    modify elements page

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,2 +1,15 @@
 from locators.elements_page_locators import TextBoxPagelocators
 from pages.base_page import BasePage
+from time import sleep
+
+class TextBoxPage(BasePage):
+    locators = TextBoxPagelocators()
+
+    def fill_all_fields(self):
+        self.element_is_visible(self.locators.FULL_NAME).send_keys('Vasiliy Petrov')
+        self.element_is_visible(self.locators.EMAIL).send_keys('Petrov@mail.com')
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('Cur address')
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Per address')
+        self.element_is_visible(self.locators.SUBMIT).click()
+
+        sleep(5)
